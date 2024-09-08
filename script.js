26278ee6b93e92ff2b3b7bf51d0de83112f2eaf5
Mohammad-arifa <ansariarif11c3a@gmail.com>
2024-09-08 20:21:00 +0530

    just added the basics of swipper from swipper.js

--- script.js
+++ script.js
@@ -40,6 +40,26 @@ menuIcon.addEventListener('click', function() {
 });
  /* nav bar endded here */
 
- 
+// swipper start here 
+ const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
